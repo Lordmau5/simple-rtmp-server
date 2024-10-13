581805c510522a4726b8aa3da5b2359ec7771442
Lordmau5 <mail@lordmau5.com>
2024-10-13 21:49:41 +0200

    Remove some things from the server code

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { UserModule } from './user/user.module';
@@ -35,7 +33,5 @@ import { MediaServerModule } from 'src/media-server/media-server.module';
 		StreamsModule,
 		MediaServerModule,
 	],
-	controllers: [AppController],
-	providers: [AppService],
 })
 export class AppModule {}
