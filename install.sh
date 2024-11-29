8b68650118a070b190683e664fa86f13ee1b9e09
Lordmau5 <mail@lordmau5.com>
2024-11-29 13:39:58 +0100

    Refactor the install script a bit more

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Set password from the first argument, default to 'admin'
-PASSWORD=${1:-admin}
+# Set password and web interface port with defaults
+read -rp "Enter the web interface password (default: admin): " PASSWORD
+PASSWORD=${PASSWORD:-admin}
+read -rp "Enter the web interface port (default: 3000): " WEB_PORT
+WEB_PORT=${WEB_PORT:-3000}
 
 # Inform user about the installation process and password
 echo "This script will install Node 20 and pm2."
-echo "The password will be set to: ${1:-admin}. Do you want to proceed? (Y/n)"
+echo "The web interface password will be set to \"$PASSWORD\" and will run on port :$WEB_PORT. Do you want to proceed? (Y/n)"
 read -r RESPONSE
 RESPONSE=${RESPONSE:-Y}  # Default response is 'Y' if not provided
 
@@ -34,8 +37,9 @@ npm install -g pm2
 cd server
 # Copy default environment config
 cp .env.default .env
-# Update password in environment file
+# Update password and web port in environment file
 sed -i "s/PASSWORD=admin/PASSWORD=$PASSWORD/" .env
+sed -i "s/WEB_PORT=3000/WEB_PORT=$WEB_PORT/" .env
 cd ..  # Return to previous directory
 
 # Start the application using pm2
@@ -43,7 +47,8 @@ pm2 start "npm start" --name "RTMP"
 pm2 save  # Save pm2 process list
 
 # Inform user that setup is complete
-echo "Setup complete, and the server has been started! Password set to: $PASSWORD"
+echo "Setup complete, and the server has been started!"
+echo "Web interface password set to \"$PASSWORD\" and web port set to :$WEB_PORT"
 
 # Reload the bash configuration to use the new settings
 exec bash
